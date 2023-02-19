fe880ccb22c032bfeeb6211e5d0d830dfd61219a
ShahirHossaini <hossainishahir@gmail.com>
2023-02-19 20:43:50 +0100

    Copy button dynamic text functionality added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,13 @@ const Header = () => {
       <aside className="title">
         <h1>box-shadow CSS Generator</h1>
       </aside>
+
       <aside className="profile">
         <div className="profile-container">
           <div className="profile-photo">
             <img src={profilePhoto} alt="shahir dev profile" />
           </div>
+
           <div className="profile-name-username">
             <p>Shahir Hussaini</p>
             <a
